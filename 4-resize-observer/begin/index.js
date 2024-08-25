d9f3e6e35b48281f08f8d4216b860fed783538db
Divine Touch <divine.touch07@gmail.com>
2024-08-25 12:43:05 -0500

    Add lesson 3 and 4

---
+++ 4-resize-observer/begin/index.js
@@ -0,0 +1,27 @@
+/*
+ * 1. Initiate Resize observer
+ * 2. Implement callback - use inlineSize and blockSize of the entries for width and height
+ * 3. If width and height is less than 150px
+ *   3.1 - Apply border-radius: 100%
+ *   3.2 - Apply border-width: 4px
+ * 4. If width and height is more than 150px = unset the properties above
+ * 5. All 4 boxes should use same observer
+ */
+
+const observer = new ResizeObserver((entries) => {
+  for (const entry of entries) {
+    const target = entry.target;
+    const box = entry.borderBoxSize[0];
+
+    if (box.blockSize < 150 && box.inlineSize < 150) {
+      target.style.borderRadius = "100%";
+      target.style.border = "4px";
+    } else {
+      target.atyle.borderRadius = "unset";
+      target.style.border = "unset";
+    }
+  }
+});
+
+const boxes = document.querySelectorAll(".box");
+boxes.forEach((b) => observer.observe(b));
